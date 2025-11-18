b291d38a5633e8d9db274c7186369eb1388f13e0
Mayank-Kumar-dev <136120115+Mayank-Kumar-dev@users.noreply.github.com>
2025-11-18 09:50:47 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,10 @@ echo "Finding imagedefinitions.json..."
 JSON_PATH=$(find /opt/codedeploy-agent/deployment-root/ -name imagedefinitions.json -print -quit)
 
 echo "Using JSON at: $JSON_PATH"
-
 IMAGE=$(jq -r '.[0].imageUri' "$JSON_PATH")
+
+echo "Logging in to Amazon ECR..."
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 029799733507.dkr.ecr.ap-south-1.amazonaws.com
 
 echo "Pulling latest image: $IMAGE"
 docker pull $IMAGE
