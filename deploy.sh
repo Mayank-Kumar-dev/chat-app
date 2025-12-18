40aa3870a71d07407cb8672d86875c17b92eb8c7
Mayank-Kumar-dev <136120115+Mayank-Kumar-dev@users.noreply.github.com>
2025-12-18 14:32:53 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+nvm install 18
+nvm use 18
+
+cd /opt/chat-app
+
+git pull origin main      # ⭐ THIS IS THE KEY
+
+npm install
+pm2 restart chat-app || pm2 start app.js --name chat-app
